63236272fab86233578b0d3b92cafd869e7fc68d
Zimolev <zimolev@gmail.com>
2023-07-13 11:36:27 +0400

    чуть исправил описание методов Calculator

--- src/main/java/org/example/SeminatFour/HomeWork/Calculator.java
+++ src/main/java/org/example/SeminatFour/HomeWork/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
     /**
      * @param numbers массив чисел
      * @return сумма всех элементов
-     * @apiNote сложение всех элементов массива
+     * @apiNote возвращающий сумму элементов коллекции
      */
     public Double sum(List<? extends Number> numbers) {
         Double result = 0.0;
@@ -17,7 +17,11 @@ public class Calculator {
         }
         return result;
     }
-
+    /**
+     * @param numbers массив чисел
+     * @return призведение всех элементов
+     * @apiNote возвращающий призведение всех элементов массива
+     */
     public Double mult(List<? extends Number> numbers) {
         Double result = 1.0;
         for (Number number : numbers) {
@@ -25,7 +29,11 @@ public class Calculator {
         }
         return result;
     }
-
+    /**
+     * @param numbers массив чисел
+     * @return деление певрвого элемента на все последующие
+     * @apiNote деление элементов массива
+     */
     public Double div(List<? extends Number> numbers) throws ArithmeticException {
         Double result = numbers.get(0).doubleValue();
         for (int i = 1; i < numbers.size(); i++) {
@@ -38,7 +46,11 @@ public class Calculator {
         }
         return result;
     }
-
+    /**
+     * @param numbers массив чисел
+     * @return массив бинаррных чисел
+     * @apiNote перевод массива в бинарный
+     */
     public List<String> binary(List<? extends Number> numbers) {
         List<String> binaryNum = new ArrayList<>();
         for (Number number : numbers) {
